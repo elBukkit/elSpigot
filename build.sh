21471944f78f74f510876e15e44b17319af0f258
Nathan Wolf <nathan@elmakers.com>
2015-03-31 21:53:07 -0700

    Add build.sh for pure Spigot

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -Rf Build
+mkdir Build
+cd Build
+curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
+export MAVEN_OPTS="-Xmx2G"
+java -Xmx2G -jar BuildTools.jar
