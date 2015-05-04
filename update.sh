fba2a3ad6f93838635ef5994c34a9aea34e8dc12
Nathan Wolf <nathan@elmakers.com>
2015-05-04 12:01:56 -0700

    Update to include 3 different PR's

--- update.sh
+++ update.sh
@@ -5,11 +5,8 @@ cd Build
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
 export MAVEN_OPTS="-Xmx2G"
 java -Xmx2G -jar BuildTools.jar
-cd Spigot
-cp ../../src/CraftMetaItem.java Spigot-Server/src/main/java/org/bukkit/craftbukkit/inventory/.
-cp ../../src/CraftMetaItemData.java Spigot-Server/src/main/java/org/bukkit/craftbukkit/inventory/.
-cp ../../src/ItemMeta.java Spigot-API/src/main/java/org/bukkit/inventory/meta/.
-cp ../../src/CraftMetaSkull.java Spigot-Server/src/main/java/org/bukkit/craftbukkit/inventory/.
+cd Spigot/Spigot-Server
+git apply ../../../patches/*
 /usr/local/maven/bin/mvn clean
 /usr/local/maven/bin/mvn install
 echo PATCH COMPLETE!
