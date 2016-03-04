12dbd3aacf9585aefdf4d9bf6057089516f6c01e
Nathan Wolf <nathan@elmakers.com>
2016-03-04 07:41:54 -0800

    I don't have any patches anymore.. just build stock spigot.

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ cd Build
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
 export MAVEN_OPTS="-Xmx2G"
 java -Xmx2G -jar BuildTools.jar --rev 1.9
-cd Spigot/Spigot-Server
-git apply ../../../patches/*
-/usr/local/maven/bin/mvn clean
-/usr/local/maven/bin/mvn install
-echo PATCH COMPLETE!
+# cd Spigot/Spigot-Server
+# git apply ../../../patches/*
+# /usr/local/maven/bin/mvn clean
+# /usr/local/maven/bin/mvn install
+# echo PATCH COMPLETE!
