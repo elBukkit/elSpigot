12f5c14b3e3374fc21849e12762b9ecae8e37769
Nathan Wolf <nathan@elmakers.com>
2019-12-14 16:24:59 -0800

    Build 1.15

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir Build
 cd Build
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
 export MAVEN_OPTS="-Xmx2G"
-java -Xmx2G -jar BuildTools.jar --rev 1.14.4
+java -Xmx2G -jar BuildTools.jar --rev 1.15
