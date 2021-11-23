f6dbe85bee8238a69c5539cde1493b4c40b609c6
Nathan Wolf <nathan@elmakers.com>
2021-11-23 14:21:46 -0800

    Build 1.18-pre5 .. going to require Java 17 on the build server though.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir Build
 cd Build
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
 export MAVEN_OPTS="-Xmx2G"
-java -Xmx2G -jar BuildTools.jar --rev 1.17.1 --remapped
+java -Xmx2G -jar BuildTools.jar --rev 1.18-pre5 --remapped
