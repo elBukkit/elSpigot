5c932b532b913b0892b6bc48fac389a3aba3e1f0
Nathan Wolf <nathan@elmakers.com>
2022-01-21 16:44:07 -0800

    No idea how this got in there

--- build.sh
+++ build.sh
@@ -5,4 +5,3 @@ cd Build
 curl https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar > BuildTools.jar
 export MAVEN_OPTS="-Xmx2G"
 java -Xmx2G -jar BuildTools.jar --rev 1.18.1 --remapped
-Built
